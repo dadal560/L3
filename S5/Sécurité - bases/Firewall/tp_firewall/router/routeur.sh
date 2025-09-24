3c78c9c41d93a88228f6c82d2b744ebc68297661
dadal560 <gwen.henry56@gmail.com>
2025-09-24 23:41:26 +0200

    lab kathara

--- S5/Sécurité - bases/Firewall/tp_firewall/router/routeur.sh
+++ S5/Sécurité - bases/Firewall/tp_firewall/router/routeur.sh
@@ -8,10 +8,16 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
 # Autoriser ICMP (ping) sur le routeur lui-même
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A OUTPUT -p icmp -j ACCEPT
 
-# Autoriser ICMP en transit (client <-> DMZ)
+#SSH depuis le client vers le routeur
+# (client = 192.168.1.1 / routeur = 192.168.1.254)
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 
-
+iptables -S
