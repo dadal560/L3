16fafe9bf22af8eb97d7bbb691736bd0a00e082c
dadal560 <gwen.henry56@gmail.com>
2025-09-30 12:33:02 +0200

    tp3 avl

---
+++ S5/Structures de données - avancé_20250915_1543/Arbres AVL/TP-AVL/debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/henry/Bureau/L3/S5/Structures de données - avancé_20250915_1543/Arbres AVL/TP-AVL/src/tree-avl
+# Build directory: /home/henry/Bureau/L3/S5/Structures de données - avancé_20250915_1543/Arbres AVL/TP-AVL/debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-tree-avl "./test-tree-avl")
+set_tests_properties(test-tree-avl PROPERTIES  _BACKTRACE_TRIPLES "/home/henry/Bureau/L3/S5/Structures de données - avancé_20250915_1543/Arbres AVL/TP-AVL/src/tree-avl/CMakeLists.txt;57;add_test;/home/henry/Bureau/L3/S5/Structures de données - avancé_20250915_1543/Arbres AVL/TP-AVL/src/tree-avl/CMakeLists.txt;0;")
