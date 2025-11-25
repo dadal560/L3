33bb67d73bf45c838e9cdd85b8c0a49193febf33
dadal <gwen.henry56@gmail.com>
2025-11-25 23:31:17 +0100

    changement image

--- S5/Programmation événementielle/TP4-20251124/TP4_base.py
+++ S5/Programmation événementielle/TP4-20251124/TP4_base.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 import MyComponents.MyWidget_exo1 as exo1
 import os
+from PyQt5.QtGui import QPixmap
 class MyImageViewerWidget(QFrame):
 
     def __init__(self, *args):
@@ -12,6 +13,10 @@ class MyImageViewerWidget(QFrame):
         self.ui = exo1.Ui_Form() 
         
         self.ui.setupUi(self)
+        
+        self.images=[]
+        self.image_index=0
+        # self.ui.mLabel.setScaledContents(True)
  
     def LoadFiles(self):
         print("Loading files...")
@@ -20,19 +25,38 @@ class MyImageViewerWidget(QFrame):
             self.ui.mLineEdit.setText(path)
             liste_fichier = os.listdir(path)
             types_fichier = ['.png', '.jpg']
-            self.images=[]
+            self.images = []
             for i in liste_fichier:
                 _, extension = os.path.splitext(i)
                 if extension in types_fichier:
                     self.images.append(i)
-            print(self.images)
+            if len(self.images) > 0:
+                self.image_index = 0
+                self.AfficherImage()
 
     def Next(self):
+        if not self.images:
+            return
+        self.image_index += 1
+        if self.image_index >= len(self.images):
+            self.image_index = 0
+        self.AfficherImage()
         print("Next image")
 
     def Previous(self):
+        if not self.images:
+            return
+        self.image_index -= 1
+        if self.image_index < 0:
+            self.image_index = len(self.images) - 1
+        self.AfficherImage()
         print("Previous image")
 
+    def AfficherImage(self, ):
+        image = self.images[self.image_index]
+        self.path_complet = os.path.join(self.ui.mLineEdit.text(), image)
+        self.px = QPixmap(self.path_complet)
+        self.ui.mLabel.setPixmap(self.px)
 
 
 
