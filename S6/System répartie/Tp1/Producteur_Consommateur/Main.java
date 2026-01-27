f5f8c1a792b1e1ea2df994ba695c53d212788771
dadal <gwen.henry56@gmail.com>
2026-01-28 00:46:06 +0100

    tp1 system repartie

--- S6/System répartie/Tp1/Producteur_Consommateur/Main.java
+++ S6/System répartie/Tp1/Producteur_Consommateur/Main.java
@@ -2,7 +2,14 @@ package Tp1.Producteur_Consommateur;
 
 public class Main {
     public static void main(String[] args) {
-        try { System.in.read(); }
+        Buffer buffer = new Buffer(10);
+        
+        new Producteur(buffer);
+
+        new Consommateur(buffer, 1); 
+        new Consommateur(buffer, 2); 
+        try { 
+            System.in.read(); }
         catch( Exception e ) { }
         System.exit(0);
     }
