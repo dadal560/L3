8e93648d8bf556117518e42f77ae1f7502abe6cf
dadal <gwen.henry56@gmail.com>
2026-01-29 23:25:30 +0100

    fin de tp system répartie

--- S6/System répartie/Tp1/Producteur_Consommateur/Main.java
+++ S6/System répartie/Tp1/Producteur_Consommateur/Main.java
@@ -3,7 +3,7 @@ package Tp1.Producteur_Consommateur;
 public class Main {
     public static void main(String[] args) {
         Buffer buffer = new Buffer(10);
-        
+
         new Producteur(buffer);
 
         new Consommateur(buffer, 1); 
